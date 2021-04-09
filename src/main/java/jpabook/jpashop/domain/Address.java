77df48ddad7439d3ba8bb83fc485aba3e2387ee8
KangSungjin <kangjoshi@nhntoast.com>
2021-04-10 00:42:14 +0900

    Lesson 3

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA 스펙상 기본 생성자가 필요하다. public 보다는 protected로 설정하여 사용하는것이 좀 더 안전하다.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
